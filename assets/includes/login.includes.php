f87b0dd3a1a0f50c5836120d5b4fa2a21ae9b7eb
aori___ <hilariomarkryan3@gmail.com>
2025-12-16 05:05:15 +0800

    feat: Add user login functionality with session management

---
+++ assets/includes/login.includes.php
@@ -0,0 +1,43 @@
+<?php 
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve username and password from form
+    $username = filter_input(INPUT_POST,"username", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST,"password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    // Proceed to the validation if the user has entered
+    // both the username and password
+    if ($username && $password) {
+        // Query to get the user
+        $sql    = "SELECT * FROM users WHERE username = ?";
+        $values = [$username];
+        $user   = execute($sql, $values)->fetch();
+
+        // Proceed to the validation if the user with the username exists and
+        // if the password if correct
+        if (isset($user->password) && password_verify($password, $user->password)) {
+            // Query for logging the current date and the time the user logged in
+            // with the session_token
+            $session_token = uniqid("user_log_");
+            $sql    = "INSERT INTO user_log (username, session_token, date, time_in) 
+                        VALUES (?, ?, CURDATE(), CURTIME());";
+            $values = [$username, $session_token];
+            execute($sql, $values);
+
+            // Store user deets in current session along
+            // with the seesion tokern yea
+            session_start();
+            $_SESSION["user"] = $user;
+            $_SESSION["session_token"] = $session_token;
+
+            // Redirect the user to the home page
+            header("location: home.php");
+        } else {
+            // Says the same thing regardless if the
+            // user doesnt exists or the password is wrong for security reasons
+            $error = "Wrong username and password.";
+        }
+
+    } else {
+        $error = "Username and password cannot be empty.";
+    }
+}
--- index.php
+++ index.php
@@ -1,51 +1,8 @@
 <?php
 
 require_once "db.php";
-require 'vendor/autoload.php';
-
-
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve username and password from form
-    $username = filter_input(INPUT_POST,"username", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $password = filter_input(INPUT_POST,"password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-
-    // Proceed to the validation if the user has entered
-    // both the username and password
-    if ($username && $password) {
-        // Query to get the user
-        $sql    = "SELECT * FROM users WHERE username = ?";
-        $values = [$username];
-        $user   = execute($sql, $values)->fetch();
-
-        // Proceed to the validation if the user with the username exists and
-        // if the password if correct
-        if (isset($user->password) && password_verify($password, $user->password)) {
-            // Query for logging the current date and the time the user logged in
-            // with the session_token
-            $session_token = uniqid("user_log_");
-            $sql    = "INSERT INTO user_log (username, session_token, date, time_in) 
-                        VALUES (?, ?, CURDATE(), CURTIME());";
-            $values = [$username, $session_token];
-            execute($sql, $values);
-
-            // Store user deets in current session along
-            // with the seesion tokern yea
-            session_start();
-            $_SESSION["user"] = $user;
-            $_SESSION["session_token"] = $session_token;
-
-            // Redirect the user to the home page
-            header("location: home.php");
-        } else {
-            // Says the same thing regardless if the
-            // user doesnt exists or the password is wrong for security reasons
-            $error = "Wrong username and password.";
-        }
-
-    } else {
-        $error = "Username and password cannot be empty.";
-    }
-}
+require_once 'vendor/autoload.php';
+require_once 'assets/includes/login.includes.php';
 
 ?>
 
@@ -54,66 +11,126 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <?php 
 
-    $page_title = "Home";
-    include "assets/components/head.php";
+$page_title = "Home";
+include "assets/components/head.php";
 
 ?>
 
 <body>
-    <!-- 
-        nav
-            uk-navbar => 
-            .uk-navbar-container => Adds the navbar background style.
+    <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky;">
+        <nav class="uk-navbar-container">
+            <div class="uk-container">
+                <div uk-navbar>
+                    <!-- Left Navigation -->
+                    <div class="uk-navbar-left">
+                        <!-- Logo -->
+                        <a class="uk-navbar-item uk-logo" href="">
+                            JDBarbers
+                        </a>
 
-            .uk-navbar-transparent => Transparent BG
-        div
-            .uk-navbar-[left, center, right] =>Aligns the navigation.
-        ul
-            .uk-navbar-nav => to create the navigation. Use <a> elements as menu items within the list.
-        li
-            .uk-parent	Add this class to indicate a parent menu item
-            .uk-active	Add this class to indicate an active menu item.
-    -->
-    <nav class="uk-navbar-container">
-        <div class="uk-container">
-            <div uk-navbar>
-                <div class="uk-navbar-left">
-                    <ul class="uk-navbar-nav">
-                        <li class="uk-logo">JDBarbers</li>
-                        <li class="uk-active"><a href="">dsad</a></li>
-                        <li class="uk-parent">
-                            <a href="#">Parent</a>
-                            <div class="uk-navbar-dropdown">
-                                <ul class="uk-nav uk-navbar-dropdown-nav">
-                                    <li class="uk-active"><a href="#">Active</a></li>
-                                    <li><a href="#">Item</a></li>
-                                    <li><a href="#">Item</a></li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="">
-                                <div>
-                                    dasds
-                                    <div class="uk-navbar-subtitle">Subtitle</div>
-                                </div>        
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="uk-navbar-center">
-                    <ul class="uk-navbar-nav">
-                        <li><a href="">IIdk</a></li>
-                    </ul>
-                </div>
-                <div class="uk-navbar-right">
-                    <ul class="uk-navbar-nav">
-                        <li><a href="">IIdk</a></li>
-                    </ul>
+                        <!-- Navigation -->
+                        <ul class="uk-navbar-nav">
+
+                            <!-- Active Button -->
+                            <li class="uk-active">
+                                <a href="">Home</a>
+                            </li>
+
+                            <!-- Dropdown -->
+                            <li class="uk-parent">
+                                <a href="#">
+                                    <div>
+                                        Parent 
+                                        <div class="uk-navbar-subtitle">1 Column</div>
+                                    </div>
+                                    <span uk-navbar-parent-icon></span>
+                                </a>
+                                <div class="uk-navbar-dropdown">
+                                    <ul class="uk-nav uk-navbar-dropdown-nav">
+
+                                        <!-- Header -->
+                                        <li class="uk-nav-header">Header</li>
+                                        <li><a href="#">Item</a></li>
+                                        <li><a href="#">Item</a></li>
+
+                                        <!-- Divider -->
+                                        <li class="uk-nav-divider"></li>
+                                        <li><a href="#">Item</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+
+                            <!-- Dropdown -->
+                            <!-- Two Column -->
+                            <li>
+                                <a href="#">
+                                    <div>
+                                        Parent
+                                        <div class="uk-navbar-subtitle">2 Columns</div>
+                                    </div>
+                                    <span uk-navbar-parent-icon></span>
+                                </a>
+                                <div class="uk-navbar-dropdown uk-navbar-dropdown-width-2">
+                                    <div class="uk-drop-grid uk-child-width-1-2" uk-grid>
+                                        <div>
+                                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                                <li class="uk-nav-header">Header</li>
+                                                <li><a href="#">Item</a></li>
+                                                <li class="uk-nav-divider"></li>
+                                                <li><a href="#">Item</a></li>
+                                            </ul>
+                                        </div>
+                                        <div>
+                                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                                <li class="uk-nav-header">Header</li>
+                                                <li><a href="#">Item</a></li>
+                                                <li class="uk-nav-divider"></li>
+                                                <li><a href="#">Item</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <!-- Center Navigation -->
+                    <div class="uk-navbar-center">
+                        <div class="uk-navbar-item">
+                            <form action="javascript:void(0)">
+                                <input class="uk-input uk-form-width-small" type="text" placeholder="Input" aria-label="Input">
+                                <button class="uk-button uk-button-default">Button</button>
+                            </form>
+                        </div>
+                    </div>
+
+                    <!-- Right Navigation -->
+                    <div class="uk-navbar-right">
+                        <ul class="uk-navbar-nav">
+                            <li>
+                                <a href="">
+                                    Profile
+                                </a>
+                            </li>
+                        </ul>
+                        
+                        <!-- Toggle -->
+                        <a class="uk-navbar-toggle" href="#">
+                            <span uk-navbar-toggle-icon></span> 
+                            <span class="uk-margin-xsmall-left">Menu</span>
+                        </a>
+                        <div class="uk-navbar-dropdown">
+                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                <li><a href="#">Active</a></li>
+                                <li><a href="#">Item</a></li>
+                                <li><a href="#">Item</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </nav>
+        </nav>
+    </div>
 
 
     <div class="uk-container uk-overflow-auto">
